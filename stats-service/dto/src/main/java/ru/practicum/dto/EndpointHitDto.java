070aea8a54425dde90b77a9957653ecbd96c07e7
Chegi21 <bilev.jenia@yandex.ru>
2025-11-22 23:43:46 +0300

    Первый коммит

---
+++ stats-service/dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -0,0 +1,28 @@
+package ru.practicum.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class EndpointHitDto {
+    private Long id;
+
+    @NotBlank(message = "Идентификатор сервиса не должен быть пустым")
+    private String app;
+
+    @NotBlank(message = "URI запроса не должен быть пустым")
+    private String uri;
+
+    @NotBlank(message = "IP-адрес пользователя не должен быть пустым")
+    private String ip;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
---
+++ stats-service/dto/src/main/java/ru/practicum/dto/ViewStatsDto.java
@@ -0,0 +1,12 @@
+package ru.practicum.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+public class ViewStatsDto {
+    private String app;
+    private String uri;
+    private Long hits;
+}
